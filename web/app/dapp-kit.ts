6c66a23e525a27b7d7ba2597f99c5c8d5b3a122d
meteorblox <faction17@gmail.com>
2026-08-13 07:11:16 -0500

    Migrate Slush wallet integration to Sui gRPC
    * Add web/app/dapp-kit.ts
    
    * Migrate web/app/providers.tsx to Sui gRPC dApp Kit
    
    * Migrate web/app/page.tsx to Sui gRPC dApp Kit
    
    * Migrate web/package.json to Sui gRPC dApp Kit

---
+++ web/app/dapp-kit.ts
@@ -0,0 +1,22 @@
+
+import { createDAppKit } from "@mysten/dapp-kit-react";
+import { SuiGrpcClient } from "@mysten/sui/grpc";
+
+const grpcUrls = {
+  testnet: "https://fullnode.testnet.sui.io:443",
+} as const;
+
+export const dAppKit = createDAppKit({
+  networks: ["testnet"],
+  defaultNetwork: "testnet",
+  createClient: (network) => new SuiGrpcClient({ network, baseUrl: grpcUrls[network] }),
+  autoConnect: true,
+  slushWalletConfig: { name: "MeteorBlox" },
+});
+
+declare module "@mysten/dapp-kit-react" {
+  interface Register {
+    dAppKit: typeof dAppKit;
+  }
+}
+

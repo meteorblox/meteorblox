57009d7a24e99753317cfcade6ef55cd5128121f
meteorblox <faction17@gmail.com>
2026-08-13 07:35:42 -0500

    List owner Testnet UpgradeCaps

--- web/app/diagnostics/page.tsx
+++ web/app/diagnostics/page.tsx
@@ -11,15 +11,17 @@ const ids = {
 };
 
 export default async function Diagnostics() {
-  const [{ object: game }, { object: refinery }, { object: upgradeCap }] = await Promise.all([
+  const [{ object: game }, { object: refinery }, { object: upgradeCap }, ownerObjects] = await Promise.all([
     client.core.getObject({ objectId: ids.game, include: { json: true } }),
     client.core.getObject({ objectId: ids.refinery, include: { json: true } }),
     client.core.getObject({ objectId: ids.upgradeCap, include: { json: true } }),
+    client.core.listOwnedObjects({ owner: "0x55f035832afb21499461d62630ed4b1cdf6e53b2a43f907e6db55a91eb114781", type: "0x2::package::UpgradeCap", include: { json: true }, limit: 50 }),
   ]);
   const data = {
     gameType: (game as { type?: string }).type ?? null,
     refineryType: (refinery as { type?: string }).type ?? null,
     upgradeCap: (upgradeCap as { json?: unknown }).json ?? null,
+    ownerUpgradeCaps: ownerObjects.objects.map((object) => ({ objectId: object.objectId, json: object.json })),
   };
   return <main><h1>METEORBLOX Testnet diagnostics</h1><pre>{JSON.stringify(data, null, 2)}</pre></main>;
 }
